a5f956143cee539804e18bc339775ca571d302fc
hanna1010 <yhn9360@gmail.com>
2020-03-14 15:54:13 +0900

    no message

--- CapstoneAR/Assets/Scenes/script/GBGmap.cs
+++ CapstoneAR/Assets/Scenes/script/GBGmap.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Text.RegularExpressions;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GBGmap : MonoBehaviour
 {
-    public Toggle toggle;
-    void start()
+    public Toggle GHMtoggle;
+    public Toggle GJJtoggle;
+    public Toggle GHRtoggle;
+    public Toggle GNJtoggle;
+
+    public void ActiveToggle() 
     {
-        toggle.onValueChange.Invoke(false);
+        if (GHMtoggle.isOn && GJJtoogle.isOn)
+        {
+            SceneManager.LoadScene("GHM-GJJ");
+        }
+
+        else if (GHMtoggle.isOn && GHRtoogle.isOn)
+        {
+            SceneManager.LoadScene("GHM-GHR");
+        }
+
+        else if (GHRtoggle.isOn && GJJtoogle.isOn)
+        {
+            SceneManager.LoadScene("GHR-GJJ");
+        }
+
+        else if (GHRtoggle.isOn && GNJtoogle.isOn)
+        {
+            SceneManager.LoadScene("GHR-GNJ");
+        }
+
+        else if (GNJtoggle.isOn && GJJtoogle.isOn)
+        {
+            SceneManager.LoadScene("GNJ-GJJ");
+        }
+
+        else
+        {
+            SceneManager.LoadScene("GHM-GJJ");
+        }
+
     }
 
-    public void toggle_Changed(bool newValue) 
+    public void OnSubmit()
     {
-        
+        ActiveToggle();
     }
 }
