440050bde03278594d32d9d5edd2ed165534f241
hanna1010 <yhn9360@gmail.com>
2020-03-14 15:21:17 +0900

    no message

---
+++ CapstoneAR/Assets/Scenes/script/GBGmap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class GBGmap : MonoBehaviour
+{
+    public Toggle toggle;
+    void start()
+    {
+        toggle.onValueChange.Invoke(false);
+    }
+
+    public void toggle_Changed(bool newValue) 
+    {
+        
+    }
+}
