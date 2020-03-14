62b581ec470bc5072ec0fa8a7e7a355e51163e2f
hanna1010 <yhn9360@gmail.com>
2020-03-14 13:16:49 +0900

    no message

---
+++ CapstoneAR/Assets/Scenes/script/SceneSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GotoStart()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    // Update is called once per frame
+    public void GotoGBG()
+    {
+        SceneManager.LoadScene("GBG");
+    }
+
+    public void GotoGHG()
+    {
+        SceneManager.LoadScene("GHG");
+    }
+
+    public void GotoCDG()
+    {
+        SceneManager.LoadScene("CDG");
+    }
+
+    public void GotoCGG()
+    {
+        SceneManager.LoadScene("CGG");
+    }
+
+    public void GotoDSG()
+    {
+        SceneManager.LoadScene("DSG");
+    }
+}
