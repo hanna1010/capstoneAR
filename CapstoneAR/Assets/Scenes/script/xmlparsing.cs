f32596d3daf6cda2aa89daf0bbeb2aac67598360
hanna1010 <yhn9360@gmail.com>
2020-03-24 17:03:00 +0900

    no message

--- CapstoneAR/Assets/Scenes/script/xmlparsing.cs
+++ CapstoneAR/Assets/Scenes/script/xmlparsing.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using System.IO;
+using System.Xml;
+
 
 public class xmlparsing : MonoBehaviour
 {
+
+    public TextAsset xmlRawFile;
+    public TextReader uiText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        string data = xmlRawFile.text;
+        parseXmlFile(data);
 
-    // Update is called once per frame
-    void Update()
+    }
+    void parseXmlFile(string xmlData)
     {
-        
+        string url = "http://www.heritage.go.kr/heri/gungDetail/gogungListOpenApi.do";
+        WWW www = new WWW(url);
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(www.text);
+
+        string xmlPathPattern = "//result/script/list";
+        XmlNodeList myNodeList = xmlDoc.SelectNodes(xmlPathPattern);
+
+        foreach (XmlNode node in myNodeList)
+        { 
+            
+        }
+
     }
 }
