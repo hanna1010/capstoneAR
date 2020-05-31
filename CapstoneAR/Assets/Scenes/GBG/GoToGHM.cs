e17fe2f1398a7d3d63d80c704ac14a739f5eef00
hanna1010 <yhn9360@gmail.com>
2020-06-01 02:18:21 +0900

    0601 ar 넣기 전

---
+++ CapstoneAR/Assets/Scenes/GBG/GoToGHM.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToGHM : MonoBehaviour
+{
+    public void GotoGHM()
+    {
+        Debug.Log("print");
+        SceneManager.LoadScene("GHM");
+    }
+    
+}
